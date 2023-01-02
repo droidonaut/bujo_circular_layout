83144d9adb4f31ab5963e15b58a6d6a6e8ebeb72
unknown <malou95@gmx.de>
2023-01-02 15:22:09 +0100

    added base template

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,13 +4,16 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/index')]
     public function number(): Response
     {
         $number = random_int(0, 100);
+
+        return $this->render('base.html.twig');
 
         return new Response(
             '<html><body>Lucky number: '.$number.'</body></html>'
